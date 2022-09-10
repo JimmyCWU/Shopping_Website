c0e02dded8dda002359d5488439f31b0135cd089
JimmyC.WU <jimmy07096@gmail.com>
2022-09-10 18:23:05 +1200

    added Swagger

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./config/swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log("Server is listen on port:", PORT);
